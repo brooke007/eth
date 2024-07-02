7a2e1ba13238247dd407644101d99be6198edcb1
brooke007 <shuzhou384@gmail.com>
2024-07-02 16:58:17 +0800

    added the quiz2 into the code

--- day1/pow.js
+++ day1/pow.js
@@ -1,14 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var CryptoJS = require("crypto-js");
-var order_id = 'brooke';
-var generated_signature = '';
-var num = 0;
+var crypto = require("crypto");
+var name = 'brooke';
+var hash = '';
+var nonce = 0;
 var zero_num = 4;
-function countLeadingZeros(str) {
+function generateKey() {
+    var _a = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        }
+    }), publicKey = _a.publicKey, privateKey = _a.privateKey;
+    return { publicKey: publicKey.toString(), PrivateKey: privateKey.toString() };
+}
+function countLeadingZeros(ans) {
     var count = 0;
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] === '0') {
+    for (var i = 0; i < ans.length; i++) {
+        if (ans[i] === '0') {
             count++;
         }
         else {
@@ -17,8 +32,40 @@ function countLeadingZeros(str) {
     }
     return count;
 }
-while (countLeadingZeros(generated_signature.toString()) < zero_num) {
-    generated_signature = CryptoJS.HmacSHA256(order_id + num.toString(), 'secret').toString(CryptoJS.enc.Hex);
-    num++;
+function signHash(name, nonce, privateKey) {
+    var data = "".concat(name).concat(nonce);
+    hash = crypto.createHash('sha256').update(data).digest('hex');
+    var sign = crypto.createSign('RSA-SHA256');
+    sign.update(hash);
+    var signature = sign.sign(privateKey, 'hex');
+    return signature;
 }
-console.log("The result is ".concat(generated_signature));
+function verifySignature(publicKey, hash, signature) {
+    var sign = crypto.createSign('RSA-SHA256');
+    sign.update(hash);
+    var verify = crypto.createVerify('RSA-SHA256');
+    verify.update(hash);
+    var isValid = verify.verify(publicKey, signature, 'hex');
+    return isValid;
+}
+function calculateNonce(name) {
+    while (countLeadingZeros(hash.toString()) < zero_num) {
+        hash = CryptoJS.HmacSHA256(name + nonce.toString(), 'secret').toString(CryptoJS.enc.Hex);
+        nonce++;
+    }
+    console.log("The result is ".concat(hash));
+    return nonce.toString();
+}
+function main() {
+    var nonce = calculateNonce(name);
+    var keypair = generateKey();
+    var publickey = keypair.publicKey;
+    var privatekey = keypair.PrivateKey;
+    var signature = signHash(name, nonce, privatekey);
+    var isvalid = verifySignature(publickey, hash, signature);
+    console.log("nonce is ".concat(nonce, "\n"));
+    console.log("".concat(publickey, "\n"));
+    console.log("hash after sign is ".concat(signature, "\n"));
+    console.log("Is the signature valid? ".concat(isvalid, "\n"));
+}
+main();
--- day1/pow.ts
+++ day1/pow.ts
@@ -1,14 +1,37 @@
 import * as CryptoJS from 'crypto-js';
+import * as crypto from 'crypto';
 
-let order_id = 'brooke';
-let generated_signature = '';
-let num = 0;
+let name = 'brooke';
+let hash = '';
+let nonce = 0;
 let zero_num = 4;
 
-function countLeadingZeros(str: string): number {
+interface KeyPair {
+    publicKey: string;
+    PrivateKey: string;
+}
+
+function generateKey(): KeyPair {
+    const { publicKey, privateKey} = crypto.generateKeyPairSync('rsa',  {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs1',
+            format: 'pem'
+        }
+    });
+
+    return { publicKey: publicKey.toString(), PrivateKey: privateKey.toString() };
+}
+
+
+function countLeadingZeros(ans: string): number {
     let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === '0') {
+    for (let i = 0; i < ans.length; i++) {
+        if (ans[i] === '0') {
             count++;
         } else {
             break;
@@ -17,9 +40,50 @@ function countLeadingZeros(str: string): number {
     return count;
 }
 
-while (countLeadingZeros(generated_signature.toString()) < zero_num) {
-    generated_signature = CryptoJS.HmacSHA256(order_id + num.toString(), 'secret').toString(CryptoJS.enc.Hex);
-    num++;
+function signHash(name: string, nonce: string, privateKey: string): string {
+    const data = `${name}${nonce}`;
+    hash = crypto.createHash('sha256').update(data).digest('hex');
+    const sign = crypto.createSign('RSA-SHA256');
+    sign.update(hash);
+    const signature = sign.sign(privateKey, 'hex');
+
+    return signature;
 }
 
-console.log(`The result is ${generated_signature}`);
+function verifySignature(publicKey: string, hash: string, signature: string): boolean {
+    const sign = crypto.createSign('RSA-SHA256');
+    sign.update(hash);
+
+    const verify = crypto.createVerify('RSA-SHA256');
+    verify.update(hash);
+
+    const isValid = verify.verify(publicKey, signature, 'hex');
+    return isValid;
+}
+
+function calculateNonce(name: string): string {
+    while (countLeadingZeros(hash.toString()) < zero_num) {
+        hash = CryptoJS.HmacSHA256(name + nonce.toString(), 'secret').toString   (CryptoJS.enc.Hex);
+        nonce++;
+    }
+    console.log(`The result is ${hash}`);
+
+    return nonce.toString();
+}
+
+function main(){
+    const nonce = calculateNonce(name);
+    const keypair = generateKey();
+    const publickey = keypair.publicKey;
+    const privatekey = keypair.PrivateKey;
+    const signature = signHash(name, nonce, privatekey);
+    const isvalid = verifySignature(publickey, hash, signature);
+
+    console.log(`nonce is ${nonce}\n`);
+    console.log(`${publickey}\n`);
+    console.log(`hash after sign is ${signature}\n`);
+    console.log(`Is the signature valid? ${isvalid}\n`);
+
+}
+
+main();
